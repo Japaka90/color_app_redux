7c6ebc3dff47ca6b83b7c4292ba821d43fb42680
Japaka90 <japaka@yandex.ru>
2018-04-08 16:06:51 +0300

    new app

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { createStore } from 'redux'
+import './App.css';
+
+
+class App extends Component {
+
+
+
+  render() {
+    return (
+      <div className="App">
+
+      </div>
+    );
+  }
+}
+
+export default App;
