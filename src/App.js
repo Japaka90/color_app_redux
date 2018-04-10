8140489cb145f04ea0c18c6a30480d807724b027
KhoAA <A.Kholodova@bssys.com>
2018-04-10 17:24:43 +0300

    simplify color block

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux'
+import { connect } from 'react-redux';
 import './App.css';
-import ColorBlock from './components/colorBlock'
-
-
+import ColorBlock from './components/colorBlock';
 
 class App extends Component {
-
   render() {
     // console.log(store.getState(), 123)
-    console.log(this.props.testStore,4);
+    console.log(this.props.store, 4);
+    const mainColor = '#ff0000';
+    const colorArray = ['#ff0000', '#1E90FF', '#EE82EE', '#F0E68C'];
     return (
       <div className="App">
-      0
-      <ColorBlock color="#ff0000" shape="round"/>
+        0
+        <div className="mainColor" style={{ backgroundColor: mainColor }} />
+        {colorArray.map(color => {
+          return <ColorBlock key={color} color={color} />;
+        })}
       </div>
-
     );
   }
 }
 
-export default connect(
-  state => ({store: state}),
-  dispatch => ({})
-)(App);
+export default connect(state => ({ store: state }), dispatch => ({}))(App);
--- src/components/colorBlock.js
+++ src/components/colorBlock.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const ColorBlock = ({color, shape, width, height}) => {
-
-  return <div
-  style={{
-      backgroundColor: color,
-      borderRadius: (shape === 'round') ? '50%' : null,
-      width: width,
-      height: height
-    }}> </div>
-}
+const ColorBlock = ({ color, width, height }) => {
+  return (
+    <div
+      style={{
+        backgroundColor: color,
+        borderRadius: '50%',
+        width: width,
+        height: height
+      }}
+    />
+  );
+};
 
 export default ColorBlock;
 
 ColorBlock.propTypes = {
   color: PropTypes.string.isRequired,
-  shape: PropTypes.string.isRequired,
   width: PropTypes.string,
   height: PropTypes.string
-}
+};
 
 ColorBlock.defaultProps = {
-  width: "20px",
-  height: "20px"
-}
+  width: '100px',
+  height: '100px'
+};
